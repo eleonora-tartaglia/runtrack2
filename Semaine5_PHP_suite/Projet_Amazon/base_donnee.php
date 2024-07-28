dcf774fbb396ee141aa77fd3383bf274c68686bb
Archimède <eleonora.tartaglia@laplateforme.io>
2024-07-28 15:08:42 +0200

    Projet de groupe Site Boutique Les Tipis Tomahawks

---
+++ Semaine5_PHP_suite/Projet_Amazon/base_donnee.php
@@ -0,0 +1,93 @@
+<?php
+
+$produits = array(
+    array(
+        "id" => 1,
+        'nom' => 'Attrape-rêves Chenoa',
+        'description' => 'Attrape-rêves avec ornements',
+        'description_detail' => "Description : Fabriqué à partir de matériaux naturels comme le bois, les pierres et les plumes, cet attrape-rêves est également orné d'un pendentif central en forme de boussole, symbolisant le guide spirituelle. C’est un talisman puissant chargé de symbolisme et d'énergie positive, il chasse les mauvais esprit pour passer une nuit magique. Parfait pour ceux qui cherchent à apporter une touche de culture amérindienne à leur intérieur tout en bénéficiant des pouvoirs protecteurs et apaisants de cet artefact traditionnel.",
+        'image' => 'images/chenoa.png',
+        'prix' => 95
+    ),
+    array(
+        "id" => 2,
+        'nom' => 'Coiffe Leïka',
+        'description' => 'Coiffe de Cérémonie avec Plumes',
+        'description_detail' => "Description : Leika est une coiffe majestueuse qui capture l'essence des cérémonies amérindiennes. Les plumes, minutieusement sélectionnées et arrangées, symbolisent la force, la sagesse et la liberté. Portée lors des grands rituels et des danses tribales, cette coiffe est un emblème de statut et de respect au sein de la communauté. Sa conception artisanale et ses matériaux naturels font de Leika une pièce unique et précieuse, idéale pour ceux qui cherchent à honorer et à perpétuer les traditions sacrées.",
+        'image' => 'images/leika.png',
+        'prix' => 155
+    ),
+    array(
+        "id" => 3,
+        'nom' => 'Mocassin Navarro',
+        'description' => 'Mocassins en Cuir avec Perles',
+        'description_detail' => "Description : Les mocassins Navarro sont une ode à l'élégance et à l'artisanat traditionnel amérindien. Confectionnés en cuir souple, ces mocassins sont ornés de perles délicates formant des motifs géométriques qui racontent des histoires ancestrales. Chaque perle est méticuleusement posée à la main, symbole de patience et de dévotion. Ces mocassins, qui allient confort et esthétique, sont parfaits pour ceux qui souhaitent marcher avec légèreté et grâce sur les sentiers sacrés.",
+        'image' => 'images/navarro.png',
+        'prix' => 135
+    ),
+    array(
+        "id" => 4,
+        'nom' => 'Panier Aiyana',
+        'description' => 'Panier Traditionnel Amérindien',
+        'description_detail' => "Description : Ce magnifique panier tressé est un véritable chef-d'œuvre de l'artisanat amérindien. Réalisé avec des fibres naturelles soigneusement sélectionnées, il arbore des motifs géométriques symboliques des traditions anciennes. Utilisé autrefois pour la cueillette et le rangement, ce panier allie beauté et fonctionnalité. Parfait pour apporter une touche d'authenticité à votre intérieur ou pour servir de pièce maîtresse dans votre décoration bohème.",
+        'image' => 'images/aiyana.png',
+        'prix' => 125
+    ),
+    array(
+        "id" => 5,
+        'nom' => 'Calumet Makawee',
+        'description' => 'Calumet de la paix',
+        'description_detail' => "Description : Le calumet de la paix Makawee est un artefact sacré et symbolique, utilisé par les peuples amérindiens pour sceller des alliances et des traités de paix. Chaque détail ayant une signification particulière : les plumes, choisies pour leur douceur et leur beauté, symbolisent les esprits protecteurs et les prières envoyées au Grand Esprit. C’est bien plus qu'un simple objet, c'est un pont entre les peuples et les esprits, un symbole de paix et de respect mutuel.",
+        'image' => 'images/makawee.png',
+        'prix' => 125
+    ),
+    array(
+        "id" => 6,
+        'nom' => 'Sacoche Dakota',
+        'description' => 'Pochette en Cuir avec Plumes',
+        'description_detail' => "Description : La pochette Dakota est une fusion élégante de robustesse et de beauté naturelle. Confectionnée en cuir véritable, elle est ornée de plumes délicates et d'un cristal brut, éléments essentiels des parures traditionnelles amérindiennes. Cette pochette est non seulement un accessoire de mode, mais aussi un symbole de connexion avec la nature et les esprits ancestraux.",
+        'image' => 'images/dakota.png',
+        'prix' => 95
+    ),
+    array(
+        "id" => 7,
+        'nom' => 'Bracelet Sequoia',
+        'description' => 'Bracelet en Cuir et Pierres',
+        'description_detail' => "Description : Le bracelet Sequoia est un trésor artisanal, fusionnant entre la robustesse du cuir et l’harmonie des nuances terrestres et célestes de chaque pierre. Le cœur en métal ajoute une note de douceur à ce bijou, rappelant les amours et les esprits bienveillants. Sequoia est plus qu'un bracelet, c'est un talisman de protection et de beauté naturelle.",
+        'image' => 'images/sequoia.png',
+        'prix' => 75
+    ),
+    array(
+        "id" => 8,
+        'nom' => 'Bague Tallulah',
+        'description' => 'Bague Totem avec Tête de Chef',
+        'description_detail' => "Description : La bague Tallulah est une pièce majestueuse, représentant la tête d'un chef amérindien. Sculptée avec précision, cette bague capture l'essence du leadership, de la sagesse et de la force. Les yeux turquoise de la sculpture évoquent les pierres sacrées utilisées dans les rituels de guérison. Porter Tallulah, c'est porter un symbole de dignité et de connexion avec les anciens et leurs enseignements.",
+        'image' => 'images/tallulah.png',
+        'prix' => 95
+    ),
+    array(
+        "id" => 9,
+        'nom' => 'Bracelet Tootsi',
+        'description' => 'Bracelet en Turquoise et Argent',
+        'description_detail' => "Description : Tootsi est un bracelet éclatant, alliant la pureté de l'argent aux tons apaisants de la turquoise. Les pierres, taillées en carrés et rectangles, sont disposées avec soin pour créer un motif harmonieux qui capte la lumière et l'attention. Ce bracelet, symbole de paix et de guérison, est parfait pour ceux qui souhaitent porter un morceau du ciel et de la terre, unissant les éléments dans une harmonie parfaite.",
+        'image' => 'images/tootsi.png',
+        'prix' => 145
+    ),
+    array(
+        "id" => 10,
+        'nom' => 'Boucle Wionnah',
+        'description' => "Boucles d'Oreilles en Plumes",
+        'description_detail' => "Description : Les boucles d'oreilles Wionnah sont des œuvres d'art aériennes, fabriquées avec des plumes délicates et des perles. Ces boucles d'oreilles capturent le mouvement et la grâce des oiseaux en vol, symbolisant la liberté et l'élégance. Les plumes, choisies pour leurs teintes vives et naturelles, dansent doucement au gré des mouvements, apportant une touche de légèreté et de mystère à chaque tenue. Wionnah est un hommage à la beauté sauvage et indomptée de la nature.",
+        'image' => 'images/wionnah.png',
+        'prix' => 85
+    ),    
+    array(
+        "id" => 11,
+        'nom' => 'Collier Quanah',
+        'description' => 'Collier de Plumes',
+        'description_detail' => "Description : Le collier Quanah est un bijou somptueux, évoquant les cérémonies rituelles et les danses sous la lune. Les plumes, choisies pour leurs couleurs chatoyantes et leur douceur, sont assemblées pour créer un collier qui célèbre la liberté et la connexion avec le ciel. Les perles et les motifs tressés ajoutent une touche de raffinement, faisant de Quanah un accessoire incontournable pour toute personne désirant honorer l'esprit des oiseaux et des anciens.",
+        'image' => 'images/quanah.png',
+        'prix' => 135
+    ),
+
+);
